4072e565805d4d9beda76795506d7a7d7623406c
Heorhii <gladiator943work@gmail.com>
2024-01-23 15:48:54 +0200

    add github.com/joho/godotenv package

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
-func Handler(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Hello, world!\n"))
+func Handler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello, world!\n"))
+}
+
+// init is invoked before main()
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
 }
 
 func main() {
+	port, portExists := os.LookupEnv("PORT")
+
+	if portExists {
+		fmt.Println(port)
+		fmt.Println(portExists)
+	} else {
+		port = "7000"
+		fmt.Println("PORT not found. Server started on reserved PORT:", port)
+	}
+
 	http.HandleFunc("/", Handler)
-	log.Println("Server is starting on PORT: 8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Println("Server is starting on PORT: " + port)
+	log.Fatal(http.ListenAndServe((":" + port), nil))
 }
